77501e8d56b085b768d06fc7245d720f5b17b218
BugHunter-X <askzenai@gmail.com>
2025-02-03 10:20:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+console.log(add("1", 2)); // Output: 3
+console.log(add(1, 2)); // Output: 3
+console.log(add("1", "2")); // Output: 3
